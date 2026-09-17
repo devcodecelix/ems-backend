a4d40c670015d539bc4dc87d9dd7206952682e7d
devcodecelix <devcodecelix@gmail.com>
2026-09-17 11:48:14 +0500

    logout correct

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -107,8 +107,9 @@ const logout = (req, res) => {
     try {
         res.clearCookie("token", {
             httpOnly: true,
-            secure: false,
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         });
         return res.status(200).json();
     } catch (err) {
